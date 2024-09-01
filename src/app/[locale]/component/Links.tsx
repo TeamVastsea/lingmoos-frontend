acf1d98747f5421f8bae86da095fcb1ff1f12f1e
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-09-01 19:38:23 +0800

    Sync links between sidebar and header

---
+++ src/app/[locale]/component/Links.tsx
@@ -0,0 +1,7 @@
+export const links = [
+    { link: '/about', label: 'features' },
+    { link: '/downloads', label: 'downloads' },
+    { link: 'https://lingmoos.github.io/lingmo-blog', label: 'blog' },
+    { link: 'https://github.com/orgs/LingmoOS/discussions/', label: 'community' },
+    { link: 'https://wiki.lingmo.org/', label: 'wiki' },
+];
