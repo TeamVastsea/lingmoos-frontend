ecea708991d70668fb140eefb6f19d6cef086ec0
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-09-01 19:07:19 +0800

    replace help with wiki

--- src/app/[locale]/component/HeaderMenu.tsx
+++ src/app/[locale]/component/HeaderMenu.tsx
@@ -13,7 +13,7 @@ const links = [
     { link: '/downloads', label: 'downloads' },
     { link: 'https://lingmoos.github.io/lingmo-blog', label: 'blog' },
     { link: 'https://github.com/orgs/LingmoOS/discussions/', label: 'community' },
-    { link: 'https://wiki.lingmo.org/', label: 'help' },
+    { link: 'https://wiki.lingmo.org/', label: 'wiki' },
 ];
 
 export function HeaderMenu(props: HeaderMenuProps) {
