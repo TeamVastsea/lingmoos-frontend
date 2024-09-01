f4adf784c718930d6a1b9f2742518156ce8ef4fc
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-09-01 20:23:08 +0800

    Update about link

--- src/app/[locale]/component/Links.tsx
+++ src/app/[locale]/component/Links.tsx
@@ -1,5 +1,5 @@
 export const links = [
-    { link: '/about', label: 'features' },
+    { link: 'https://lingmoos.github.io/lingmo-blog/about', label: 'features' },
     { link: '/downloads', label: 'downloads' },
     { link: 'https://lingmoos.github.io/lingmo-blog', label: 'blog' },
     { link: 'https://github.com/orgs/LingmoOS/discussions/', label: 'community' },
